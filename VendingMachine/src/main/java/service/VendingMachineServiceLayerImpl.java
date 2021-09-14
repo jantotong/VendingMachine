39340a1473b012f810e46b6efdb3921f1a5b4515
Pierre L'Espérance <polpolzor@hotmail.com>
2021-09-14 17:49:25 -0400

    another commit POL

--- VendingMachine/src/main/java/service/VendingMachineServiceLayerImpl.java
+++ VendingMachine/src/main/java/service/VendingMachineServiceLayerImpl.java
@@ -5,6 +5,7 @@
  */
 package service;
 
+import dao.VendingMachineAuditDao;
 import dao.VendingMachineDao;
 import dto.Product;
 
@@ -16,18 +17,57 @@ import java.util.List;
 
 public class VendingMachineServiceLayerImpl implements VendingMachineServiceLayer {
 
-    VendingMachineDao dao;
+    private VendingMachineDao dao;
+    private VendingMachineAuditDao auditDao;
+    
 
     public VendingMachineServiceLayerImpl(VendingMachineDao dao) {
         this.dao = dao;
     }
-
-    @Override
-    public Product addProduct(String name, Product product) throws InsufficientFundsException, NoItemInventoryException, VendingMachineDuplicateNameException, VendingMachinePersistenceException {
-        //if(dao.getProduct(name)){}
-        throw new UnsupportedOperationException("No Support Yet");
+    
+    public VendingMachineServiceLayerImpl(VendingMachineDao dao, VendingMachineAuditDao auditDao) {
+        this.dao = dao;
+        this.auditDao = auditDao;
     }
 
+    @Override
+    public void addProduct(String name, Product product) throws VendingMachineDataValidationException, NoItemInventoryException, VendingMachineDuplicateNameException, VendingMachinePersistenceException 
+    {
+        //if(dao.getProduct(name)){}
+
+        // First check to see if there is alreay a student 
+        // associated with the given student's id
+        // If so, we're all done here - 
+        // throw a ClassRosterDuplicateIdException
+        
+        // VendingMachinePersistenceException
+        
+        
+        
+        
+        if (dao.getProduct(name) != null) {
+            throw new VendingMachineDuplicateNameException(
+                    "ERROR: Could not create item named "
+                    + product.getName()
+                    + " already exists");
+        }
+
+        // Now validate all the fields on the given Student object.  
+        // This method will throw an
+        // exception if any of the validation rules are violated.
+        validateProductData(product);
+
+        // We passed all our business rules checks so go ahead 
+        // and persist the Student object
+        dao.addProduct(name, product);
+
+        // The student was successfully created, now write to the audit log
+        auditDao.writeAuditEntry(
+                "Product " + product.getName() + " CREATED.");
+
+
+    }
+    
     @Override
     public List<Product> DisplayAllProduct() throws NoItemInventoryException, VendingMachinePersistenceException {
         throw new UnsupportedOperationException("No Support Yet");
