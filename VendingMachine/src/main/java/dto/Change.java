ba7e80723453adcdd4a11135f044c3fa787a8591
jantotong <tongjantoo@gmail.com>
2021-09-15 09:36:04 -0700

    Change
    public Change (BigDecimal total)
    public Map<changesType, BigDecimal> exchangeTotal()

--- VendingMachine/src/main/java/dto/Change.java
+++ VendingMachine/src/main/java/dto/Change.java
@@ -5,10 +5,40 @@
  */
 package dto;
 
+import java.math.*;
+import java.util.*;
+
 /**
  *
  * @author Noah McElroy
  */
 public class Change {
-    
+
+    private BigDecimal total;
+    private BigDecimal quarterValue = new BigDecimal("25");
+    private BigDecimal dimeValue = new BigDecimal("10");
+    private BigDecimal nickelValue = new BigDecimal("5");
+
+    Change (BigDecimal total){
+        this.total = total;
+    }
+
+    private enum changesType {
+        QUARTERS, DIMES, NICKELS, PENNIES
+    }
+
+    public Map<changesType, BigDecimal> exchangeTotal(){
+        BigDecimal current = total;
+        Map<changesType, BigDecimal> pocket = new HashMap<>();
+
+        pocket.put(changesType.QUARTERS, current.divide(quarterValue, 0, RoundingMode.DOWN));
+        current = current.remainder(quarterValue);
+        pocket.put(changesType.DIMES, current.divide(dimeValue, 0, RoundingMode.DOWN));
+        current = current.remainder(dimeValue);
+        pocket.put(changesType.NICKELS, current.divide(nickelValue, 0, RoundingMode.DOWN));
+        current = current.remainder(nickelValue);
+        pocket.put(changesType.PENNIES, current);
+        return pocket;
+    }
+
 }
